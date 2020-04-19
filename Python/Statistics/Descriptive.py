93206d573f216d17d5582e527d0bb3b288ae9b7e
Philip Rothschild <philrr16@uia.no>
2020-04-20 00:12:05 +0200

    Add: Separating code related to Descriptive and Inferential Statistics

---
+++ Python/Statistics/Descriptive.py
@@ -0,0 +1,7 @@
+import numpy as np
+import stat
+
+class Descriptive:
+    def __init__(self) -> None:
+        super().__init__()
+
---
+++ Python/Statistics/Inference.py
@@ -0,0 +1,6 @@
+import numpy as np
+import stat
+
+class Inference:
+    def __init__(self) -> None:
+        super().__init__()
